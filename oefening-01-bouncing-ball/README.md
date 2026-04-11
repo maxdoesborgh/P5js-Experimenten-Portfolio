a7b1dd4f89c426cf20ad6487778ecdfe06f89298
Max Doesborgh <max.doesborgh1983@gmail.com>
2026-04-11 13:33:37 +0200

    feat: voeg stap kleurverandering bij botsing toe aan bouncing ball

---
+++ oefening-01-bouncing-ball/README.md
@@ -0,0 +1,6 @@
+Stap 1: Volledige horizontale botsing
+* Wat heb ik gedaan? Ik heb een tweede voorwaarde toegevoegd aan het if-statement.
+* Techniek: Door de OR-operator (||) te gebruiken, controleert de computer nu of de bal de rechterrand (width) óf de linkerrand (0) raakt.
+
+Geleerd: Ik begrijp nu dat speed * -1 een positief getal negatief maakt, en een negatief getal weer positief. Dit zorgt voor het "stuiter" effect.
+
--- oefening-01-bouncing-ball/sketch.js
+++ oefening-01-bouncing-ball/sketch.js
@@ -1,19 +1,28 @@
-var x = 0;
-var speed = 3;
+let x = 0;
+let r = 255;
+let g = 255;
+let b = 255;
+let speed = 3;
 
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(0);
+background(0);
+  
+  if (x > width || x < 0) {
+    speed = speed * -1; 
+
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
+
+  x = x + speed;
+
   stroke(255);
   strokeWeight(4);
-  noFill();
+  fill(r, g, b); 
   ellipse(x, 200, 100, 100);
-
-  if (x > width || x < 0) {
-  speed = speed * -1;
-}
-  x = x + speed;
 }
